2cbd9699343da21ae41b8ebfeeab90cc6038c0dc
Jingyi Zhang <zhan139@usc.edu>
2020-09-07 20:07:09 -0700

    First Stage
    Blade Slice implemented.
    Art assets imported

---
+++ Assets/Blade.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject bladeTrailPrefab;
+
+    [SerializeField]
+    public float minCuttingVelocity = 1.0f;
+
+    bool isCutting = false;
+
+    Vector2 prevPos;
+
+    GameObject currentBladeTrail;
+    Rigidbody2D rb;
+    Camera cam;
+    CircleCollider2D circleCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+        circleCollider = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            StartCutting();
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            StopCuttting();
+        }
+
+        if(isCutting)
+        {
+            UpdateCut();
+        }
+    }
+
+    void StartCutting()
+    {
+        isCutting = true;
+        currentBladeTrail = Instantiate(bladeTrailPrefab, transform);
+        circleCollider.enabled = false;
+        prevPos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void StopCuttting()
+    {
+        isCutting = false;
+        currentBladeTrail.transform.SetParent(null);
+        Destroy(currentBladeTrail, 2.0f);
+        circleCollider.enabled = false;
+    }
+
+    void UpdateCut()
+    {
+
+        Vector2 newPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        rb.position = newPos;
+
+        float velocity = (newPos - prevPos).magnitude / Time.deltaTime;
+
+        prevPos = newPos;
+
+        if(velocity > minCuttingVelocity)
+        {
+            circleCollider.enabled = true;
+        }
+        else
+        {
+            circleCollider.enabled = false;
+        }
+    }
+
+
+}
---
+++ Assets/ScareCrowMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScareCrowMovement : MonoBehaviour
+{
+    [SerializeField] public float speed;
+    private Rigidbody2D myrigidbody;
+    private Vector3 mousechange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myrigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        mousechange = Vector2.zero;
+        mousechange.x = Input.GetAxisRaw("Horizontal");
+        mousechange.y = Input.GetAxisRaw("Vertical");
+       
+        if(mousechange != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+
+    void MoveCharacter()
+    {
+        myrigidbody.MovePosition(transform.position + mousechange.normalized * speed * Time.deltaTime);
+    }
+}
