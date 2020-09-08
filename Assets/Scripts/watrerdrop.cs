b69abb58a8514f44b4a963713f56c43387328ba8
Jingyi Zhang <zhan139@usc.edu>
2020-09-08 03:17:45 -0700

    Particle System implemented
    All paricles are implemented

---
+++ Assets/Scripts/watrerdrop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class watrerdrop : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject waterParticlePrefab;
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Blade")
+        {
+            GameObject particle = Instantiate(waterParticlePrefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
